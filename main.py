3b736746739d556307d34cdca709f1fe22bbd0b8
bobini <tomasz.kalisiak3@gmail.com>
2021-10-15 00:17:41 +0200

    Incorporated Raspberry Pi pin bindings.

--- main.py
+++ main.py
@@ -1,39 +1,68 @@
+from datetime import datetime
 import discord
 from dotenv import load_dotenv
 import os
 from discord.ext import commands
 import shelve
-import asyncio
-import random
 from operator import itemgetter
+import RPi.GPIO as GPIO
+from functools import partial
+import pickle as pkl
 
 
 class DBManager:
     def __init__(self, client):
         self.client = client
-        self.timestamp = "19:29"
 
-    async def update_timestamp(self):
-        await asyncio.sleep(10)
-        return f"{random.randint(1, 24)}:{random.randint(0, 59)}"
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(5, GPIO.IN)
 
-    async def run(self):
+        with open('detectorstate.pkl', 'rb') as f:
+            try:
+                self.timestamp, self.state = itemgetter("timestamp", "state")(pkl.load(f))
+            except KeyError:
+                self.timestamp, self.state = str(datetime.now()), bool(GPIO.input(5))
+
         for guild in self.client.guilds:
-            await send_embed(self.client, guild, self.timestamp, edit=True)
-        while True:
-            self.timestamp = await self.update_timestamp()
-            for guild in self.client.guilds:
-                await send_embed(self.client, guild, self.timestamp, edit=True)
+            await send_embed(self.state, self.client, guild, self.timestamp, edit=True)
+
+        @client.event
+        async def on_message(msg):
+            if msg.author != client.user:
+                with shelve.open('db') as db:
+                    check_result = str(msg.guild.id) in db and msg.channel.id == db[str(msg.guild.id)][
+                        "selected_channel"]
+                if check_result:
+                    await send_embed(self.state, self.client, msg.guild, self.timestamp, edit=False)
+            await client.process_commands(msg)
+
+        GPIO.add_event_detect(5, GPIO.RISING, callback=partial(self.state_change, True))
+        GPIO.add_event_detect(5, GPIO.FALLING, callback=partial(self.state_change, False))
+
+    def state_change(self, entered):
+        self.state = entered
+        self.timestamp = str(datetime.now())
+
+        with open('detectorstate.pkl', 'wb') as f:
+            pkl.dump({"timestamp": self.timestamp, "state": self.state}, f)
+
+        for guild in self.client.guilds:
+            await send_embed(entered, self.client, guild, self.timestamp, edit=True)
 
 
-async def send_embed(client, guild, timestamp, edit=False):
-    send = False
+async def send_embed(entered, client, guild, timestamp, edit=False):
     with shelve.open('db', writeback=True) as db:
         if str(guild.id) in db:
 
-            embed = discord.Embed(title="Ostatnia wizyta w siedzibie koła")
+            if entered is None:
+                embed = discord.Embed(title="Unknown state")
+            elif entered:
+                embed = discord.Embed(title="Siedziba koła otwarta od: ")
+            else:
+                embed = discord.Embed(title="Siedziba koła zamknięta od: ")
+
             embed.set_author(name="SKN main bot")
-            embed.add_field(name="Godzina", value=timestamp, inline=False)
+            embed.add_field(name="Data", value=timestamp, inline=False)
 
             if db[str(guild.id)]["last_message"] is not None:
                 channelID, messageID = itemgetter("channel", "message")(db[str(guild.id)]["last_message"])
@@ -63,15 +92,6 @@ def main():
 
     client = commands.Bot(command_prefix="!")
 
-    @client.event
-    async def on_message(msg):
-        if msg.author != client.user:
-            with shelve.open('db') as db:
-                check_result = str(msg.guild.id) in db and msg.channel.id == db[str(msg.guild.id)]["selected_channel"]
-            if check_result:
-                await send_embed(client, msg.guild, "test")
-        await client.process_commands(msg)
-
     @client.command(name="setchannel")
     async def set_channel(ctx, arg):
         with shelve.open('db', writeback=True) as db:
@@ -83,8 +103,7 @@ def main():
     @client.event
     async def on_ready():
         print(f'{client.user} has connected to Discord!')
-        manager = DBManager(client)
-        asyncio.create_task(manager.run())
+        DBManager(client)
 
     client.run(TOKEN)
 
